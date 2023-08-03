3582fa482a720c24f6492cfccb5d66e58824833f
lala9663 <lala96632040@gmail.com>
2023-08-04 01:07:36 +0900

    member repository

---
+++ src/main/java/wanted/project/wantedpreonboardingbackend/member/repository/MemberRepository.java
@@ -0,0 +1,14 @@
+package wanted.project.wantedpreonboardingbackend.member.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import wanted.project.wantedpreonboardingbackend.member.entity.Member;
+
+import java.util.Optional;
+
+@Repository
+public interface MemberRepository extends JpaRepository<Member, Long> {
+    Optional<Member> findByEmail(String email);
+
+    boolean existsByEmail(String email);
+}
