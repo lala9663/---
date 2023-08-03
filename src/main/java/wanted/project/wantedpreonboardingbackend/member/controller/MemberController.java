16cf1b7dd8ef9abce1f7b9283c4ce726b2347807
lala9663 <lala96632040@gmail.com>
2023-08-04 02:19:25 +0900

    feat: controller 응답값 변경 및 로그아웃, 관리자 권한 controller 추가

--- src/main/java/wanted/project/wantedpreonboardingbackend/member/controller/MemberController.java
+++ src/main/java/wanted/project/wantedpreonboardingbackend/member/controller/MemberController.java
@@ -5,15 +5,15 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.Errors;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import wanted.project.wantedpreonboardingbackend.member.dto.request.LoginRequestDto;
+import wanted.project.wantedpreonboardingbackend.member.dto.request.LogoutRequestDto;
 import wanted.project.wantedpreonboardingbackend.member.dto.request.SignUpRequestDto;
 import wanted.project.wantedpreonboardingbackend.member.dto.response.LoginResponseDto;
+import wanted.project.wantedpreonboardingbackend.member.dto.response.Response;
 import wanted.project.wantedpreonboardingbackend.member.dto.response.SignUpResponseDto;
 import wanted.project.wantedpreonboardingbackend.member.service.MemberService;
+import wanted.project.wantedpreonboardingbackend.security.lib.Helper;
 
 @RestController
 @RequestMapping("/members")
@@ -21,20 +21,34 @@ import wanted.project.wantedpreonboardingbackend.member.service.MemberService;
 @RequiredArgsConstructor
 public class MemberController {
     private final MemberService memberService;
+    private final Response response;
+
     @PostMapping("/signup")
-    public ResponseEntity<SignUpResponseDto> signUp(@Validated @RequestBody SignUpRequestDto signUpRequestDto, Errors errors) {
+    public ResponseEntity<?> signUp(@Validated SignUpRequestDto signUp, Errors errors) {
 
         if (errors.hasErrors()) {
-            return ResponseEntity.badRequest().build();
+            return response.invalidFields(Helper.refineErrors(errors));
         }
 
-        SignUpResponseDto responseDto = memberService.signup(signUpRequestDto);
-
-        return ResponseEntity.ok(responseDto);
+        return memberService.signup(signUp);
     }
     @PostMapping("/login")
-    public ResponseEntity<LoginResponseDto> login(@RequestBody LoginRequestDto loginRequestDto) {
-        LoginResponseDto responseDto = memberService.login(loginRequestDto);
-        return ResponseEntity.ok(responseDto);
+    public ResponseEntity<?> login(@Validated LoginRequestDto login, Errors errors) {
+        if (errors.hasErrors()) {
+            return response.invalidFields(Helper.refineErrors(errors));
+        }
+        return memberService.login(login);
+    }
+    @PostMapping("/logout")
+    public ResponseEntity<?> logout(@Validated LogoutRequestDto logout, Errors errors) {
+        if (errors.hasErrors()) {
+            return response.invalidFields(Helper.refineErrors(errors));
+        }
+        return memberService.logout(logout);
+    }
+    @GetMapping("/authority")
+    public ResponseEntity<?> authority() {
+        log.info("ADD ROLE_ADMIN");
+        return memberService.authority();
     }
 }
