06ee18d08cfedffda583d96f245d706f34d5c122
lala9663 <lala96632040@gmail.com>
2023-08-04 01:45:16 +0900

    UsernamePasswordAuthenticationToken 객체 생성

--- src/main/java/wanted/project/wantedpreonboardingbackend/member/dto/request/LoginRequestDto.java
+++ src/main/java/wanted/project/wantedpreonboardingbackend/member/dto/request/LoginRequestDto.java
@@ -1,6 +1,7 @@
 package wanted.project.wantedpreonboardingbackend.member.dto.request;
 
 import lombok.*;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 
 @Getter
 @Setter
@@ -11,4 +12,8 @@ import lombok.*;
 public class LoginRequestDto {
     private String email;
     private String password;
+
+    public UsernamePasswordAuthenticationToken toAuthentication() {
+        return new UsernamePasswordAuthenticationToken(email, password);
+    }
 }
