801520ba89d8e60647478a8a912e020873cffa02
lala9663 <lala96632040@gmail.com>
2023-08-06 18:30:52 +0900

    빌더 직접 생성으로 변경

--- src/main/java/wanted/project/wantedpreonboardingbackend/board/dto/request/CreateBoardDto.java
+++ src/main/java/wanted/project/wantedpreonboardingbackend/board/dto/request/CreateBoardDto.java
@@ -1,15 +1,24 @@
 package wanted.project.wantedpreonboardingbackend.board.dto.request;
 
 import lombok.*;
+import wanted.project.wantedpreonboardingbackend.board.entity.Board;
+import wanted.project.wantedpreonboardingbackend.member.entity.Member;
 
 @Getter
 @Setter
 @ToString
 @NoArgsConstructor
 @AllArgsConstructor
-@Builder
 public class CreateBoardDto {
     private String title;
     private String content;
-    private Long writer;
+
+    public Board toEntity(Member member) {
+        return Board.builder()
+                .member(member)
+                .title(title)
+                .content(content)
+                .build();
+    }
+
 }
